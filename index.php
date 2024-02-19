6f2776431158d260028a53ab0b4d574e42d7e68e
NZEKWEKINGSLEYCHUKWUDI1 <160473453+NZEKWEKINGSLEYCHUKWUDI1@users.noreply.github.com>
2024-02-19 10:16:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Nzekwe Kingsley Chukwudi";
+$regNo = "2022/HND/35472/CS";
+$stateOfOrigin = "Imo";
+$localGovt = "Okigwe";
+$age = 27;
+
+echo "Name: $name<br>";
+echo "Registration Number: $regNo<br>";
+echo "State of Origin: $stateOfOrigin<br>";
+echo "Local Government Area: $localGovt<br>";
+echo "Age: $age years old";
+?>
